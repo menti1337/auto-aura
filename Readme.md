4beab29d8533831735efdf48a8baf6c6beb8cceb
Dmitry Shin <dmitry.s@hsdn.org>
2021-11-10 00:37:41 +0400

    GF v110.3

--- Readme.md
+++ Readme.md
@@ -1,7 +1,9 @@
 # auto-aura
 Automatically enables auras (crit, mana and thrall) when playing mystic.
 
-# Commands
-- `autoaura` to toggle module (default: ON)
-- `autoaura pvp` to toggle enabling aura for PvP instead of PvE (default: OFF)
-- `autoaura onrez` to toggle automated recast of auras on rez (warning: it causes undesirable animation lock on rez, use at your own risk) (default: OFF)
+## Commands
+
+Toolbox(/8) | Command description
+--- | ---
+**autoaura** | Module on/off (default: on).
+**autoaura pvp** | Enabling aura for PvP instead of PvE (default: off).
--- index.js
+++ index.js
@@ -1,123 +1,142 @@
-module.exports = function auto_auras(mod) {
-	const command = mod.command;
-	const AbnormalManager = require("./lib/abnormal");
-	const AbnManager = new AbnormalManager(mod, false);
-	let loc = null,
-		wloc = null,
-		timer = null,
-		isMystic = false,
-		isDead = false;
+"use strict";
 
-	command.add(["autoaura", "!autoaura"], {
+const Vec3 = require("tera-vec3");
+
+module.exports = function autoAuras(mod) {
+	let locking = false;
+	let lockingTimerId = null;
+	let playerLocation = null;
+	let playerDirection = null;
+	let timerId = null;
+	let intervalId = null;
+
+	mod.game.initialize("me.abnormalities");
+
+	mod.command.add("autoaura", {
 		$none() {
 			mod.settings.enabled = !mod.settings.enabled;
-			command.message(`auto-aura ${mod.settings.enabled ? "Enabled" : "Disabled"}.`);
-		},
-		onrez() {
-			mod.settings.onrez = !mod.settings.onrez;
-			command.message(`auto-aura on rez ${mod.settings.onrez ? "Enabled" : "Disabled"}.`);
+			mod.command.message(`auto-aura ${mod.settings.enabled ? "Enabled" : "Disabled"}.`);
 		},
 		pvp() {
 			mod.settings.pvp = !mod.settings.pvp;
-			command.message(`auto-aura PvP ${mod.settings.pvp ? "Enabled" : "Disabled"}.`);
+			mod.command.message(`auto-aura PvP ${mod.settings.pvp ? "Enabled" : "Disabled"}.`);
 		},
 		$default() {
-			command.message("Error (typo?) in command! see README for the list of valid commands");
+			mod.command.message("Error (typo?) in command! see README for the list of valid commands");
 		}
+	});
+
+	mod.hook("S_SPAWN_ME", 3, { "order": Infinity }, event => {
+		playerLocation = event.loc;
+		playerDirection = event.w;
+
+		if (isEnabled() && event.alive) {
+			mod.setTimeout(() => enableAuras(), 3000);
+		}
+	});
+
+	mod.hook("C_PLAYER_LOCATION", 5, { "order": Infinity }, event => {
+		playerLocation = event.loc;
+		playerDirection = event.w;
+	});
+
+	mod.hook("S_ACTION_STAGE", 9, { "order": -Infinity, "filter": { "fake": null } }, event => {
+		if (!isEnabled() || !mod.game.me.is(event.gameId)) return;
+
+		mod.clearTimeout(lockingTimerId);
+		locking = true;
+	});
+
+	mod.hook("S_ACTION_END", 5, { "order": -Infinity, "filter": { "fake": null } }, event => {
+		if (!isEnabled() || !mod.game.me.is(event.gameId)) return;
+
+		mod.clearTimeout(lockingTimerId);
+		lockingTimerId = mod.setTimeout(() => locking = false, 300);
+	});
+
+	mod.hook("C_CANCEL_SKILL", 3, { "order": -Infinity }, () => {
+		if (!isEnabled()) return;
+
+		mod.clearTimeout(lockingTimerId);
+		locking = false;
 	});
 
 	function isEnabled() {
-		return mod.settings.enabled && isMystic;
+		return mod.settings.enabled && mod.game.me.class === "elementalist";
 	}
 
-	mod.game.on("enter_game", () => {
-		const model = mod.game.me.templateId;
-		const job = (model - 10101) % 100;
-		isMystic = (job == 7);
-		if (isEnabled()) {
-			command.message("Enabling auto-auras");
-		}
-	});
+	function enableAuras() {
+		if (intervalId) return;
 
-	async function auras() {
-		// Aura of the Merciless
-		if (hasNoAbn([700600, 700601, 700602, 700603])) {
-			startSkill(130400);
-		}
-		// Aura of the Unyielding
-		if (hasNoAbn([700203, 700233]) && mod.settings.pvp) {
-			startSkill(150400);
-		}
-		// Aura of the Tenacious
-		if (hasNoAbn([700330, 700300]) && !mod.settings.pvp) {
-			startSkill(160100);
-		}
-		// Thrall Augmentation
-		if (hasNoAbn([702000, 702005])) {
-			startSkill(450100);
-		}
-		if (!hasNoAbn([700600, 700601, 700602, 700603]) &&
-			!hasNoAbn([700203, 700233, 700330, 700300]) &&
-			!hasNoAbn([702000, 702005])
-		) {
-			mod.clearInterval(timer);
-		}
+		mod.clearTimeout(timerId);
+		timerId = null;
+
+		mod.command.message("Enabling auto-auras");
+
+		intervalId = mod.setInterval(() => {
+			// Aura of the Merciless
+			if (hasNoAbn([700600, 700601, 700602, 700603])) {
+				startSkill(130400);
+			}
+
+			// Aura of the Unyielding
+			if (hasNoAbn([700203, 700233]) && mod.settings.pvp) {
+				startSkill(150400);
+			}
+
+			// Aura of the Tenacious
+			if (hasNoAbn([700330, 700300]) && !mod.settings.pvp) {
+				startSkill(160100);
+			}
+
+			// Thrall Augmentation
+			if (hasNoAbn([702000, 702005])) {
+				startSkill(450100);
+			}
+
+			if (!hasNoAbn([700600, 700601, 700602, 700603]) &&
+				!hasNoAbn([700203, 700233, 700330, 700300]) &&
+				!hasNoAbn([702000, 702005])
+			) {
+				mod.clearInterval(intervalId);
+				intervalId = null;
+			}
+		}, 100);
+
+		timerId = mod.setTimeout(() => {
+			if (intervalId) {
+				mod.clearInterval(intervalId);
+				intervalId = null;
+			}
+		}, 8000);
 	}
 
-	function startSkill(skillId) { //not all skill types supported
-		mod.toServer("C_START_SKILL", 7, {
-			skill: { reserved: 0, npc: false, type: 1, huntingZoneId: 0, id: skillId },
-			w: wloc,
-			loc: loc,
-			dest: { x: 0, y: 0, z: 0 },
-			unk: true,
-			moving: false,
-			continue: false,
-			target: 0n,
-			unk2: false
+	function hasNoAbn(abnormalities) {
+		return Object.keys(mod.game.me.abnormalities).filter(a => abnormalities.includes(Number(a))).length === 0;
+	}
+
+	function startSkill(skillId) {
+		if (locking || !playerLocation) return;
+
+		mod.clearTimeout(lockingTimerId);
+		locking = true;
+
+		mod.send("C_START_SKILL", 7, {
+			"skill": {
+				"reserved": 0,
+				"npc": false,
+				"type": 1,
+				"huntingZoneId": 0,
+				"id": skillId
+			},
+			"loc": playerLocation,
+			"w": playerDirection,
+			"dest": new Vec3(0, 0, 0),
+			"unk": true,
+			"moving": false,
+			"continue": false,
+			"target": 0n,
+			"unk2": false
 		});
 	}
-
-	function hasNoAbn(ids) {
-		return ids.reduce((accumulator, currentId) => accumulator && !hasAbn(currentId), true);
-	}
-
-	function hasAbn(id) {
-		return ("added" in AbnManager.get(mod.game.me.gameId, id));
-	}
-
-	mod.hook("S_SPAWN_ME", 3, (event) => {
-		if (mod.game.me.gameId == event.gameId && !mod.game.me.inOpenWorld) {
-			loc = event.loc;
-			wloc = event.w;
-			if (isEnabled()) {
-				mod.clearInterval(timer);
-				mod.setTimeout(() => {
-					timer = mod.setInterval(auras, 500);
-				}, 3000);
-			}
-		}
-	});
-
-	mod.hook("C_PLAYER_LOCATION", 5, (event) => {
-		loc = event.loc;
-		wloc = event.w;
-	});
-
-	mod.hook("S_CREATURE_LIFE", 3, (event) => {
-		if (isEnabled() && mod.settings.onrez) {
-			if (event.gameId !== mod.game.me.gameId || mod.game.me.inOpenWorld) return;
-			loc = event.loc;
-			mod.clearInterval(timer);
-			if (!event.alive) {
-				isDead = true;
-			} else if (isDead) {
-				isDead = false;
-				mod.setTimeout(() => {
-					startSkill(450100); // Thrall Augmentation enabling fix
-					timer = mod.setInterval(auras, 500);
-				}, 3000);
-			}
-		}
-	});
 };
